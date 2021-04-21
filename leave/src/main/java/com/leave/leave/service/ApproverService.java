5035b5c850a998384eeb5bc7d039cccd7b817d02
RamireddySandhya <69138141+RamireddySandhya@users.noreply.github.com>
2021-04-21 11:21:43 +0530

    Delete leave directory

--- leave/src/main/java/com/leave/leave/service/ApproverService.java
+++
@@ -1,9 +0,0 @@
-package com.leave.leave.service;
-
-import org.springframework.stereotype.Service;
-
-import com.leave.leave.model.ApprovalInfo;
-@Service
-public interface ApproverService {
-	public ApprovalInfo save(ApprovalInfo approver);
-}
